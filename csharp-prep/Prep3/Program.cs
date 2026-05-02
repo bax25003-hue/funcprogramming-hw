ef45d4aea38f842d674ddbd9863d1b3ad97fb496
bax25003-hue <bax25003@byui.edu>
2026-05-02 15:42:58 -0600

    Prep 3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Initial magic number variable
+        int magicNumber = Random.Shared.Next(1,101);
+
+        // Guess loop
+        int guess;
+        do
+        {
+        string guessInput;
+        Console.Write("What is your guess? ");
+        guessInput = Console.ReadLine();
+        guess = int.Parse(guessInput);
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+        while (guess != magicNumber);
+        
     }
 }
